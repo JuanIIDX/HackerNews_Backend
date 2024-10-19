413dd231afedd8bf1f6fc9474a84b3f43bd526e6
Juan <juan.1701523251@ucaldas.edu.co>
2024-10-19 14:27:33 -0500

    orden de archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './config';
+import { News } from './news/entities/news.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: config.DATABASE_PORT,
+      host: config.DATABASE_HOST,
+      database: config.DATABASE_NAME,
+      username: config.DATABASE_USERNAME,
+      password: config.DATABASE_PASSWORD,
+      entities: [News
+       ],
+      synchronize: true,
+  }),
+    NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    DATABASE_HOST: 'localhost',
+    DATABASE_PORT: 5432,
+    DATABASE_USERNAME: 'postgres',
+    DATABASE_PASSWORD: '3245234234',
+    DATABASE_NAME: 'HackNews',
+  };
+  
